b7c286c6f7fdb8f7a6fb3da591393da3802596dd
Sean Story <sean.j.story@gmail.com>
2020-04-09 16:56:11 -0500

    Initial Commit

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/filesystem/document/HelloWorldDocument.java
@@ -0,0 +1,25 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.filesystem.document;
+
+import com.sstory.workplace.search.sdk.api.DocumentBase;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class HelloWorldDocument extends DocumentBase {
+
+    public HelloWorldDocument(String id) {
+        super(id, "hello, world!");
+    }
+
+    public HelloWorldDocument(String id, String body){
+        super(id, body);
+    }
+
+    @Override
+    public Map<String, Object> getMetadata() {
+        return Collections.emptyMap();
+    }
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/sources/HelloWorldSource.java
@@ -0,0 +1,44 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.sources;
+
+import com.sstory.workplace.search.sdk.api.Source;
+import com.sstory.workplace.search.sdk.api.Yielder;
+import ${package}.document.HelloWorldDocument;
+
+import java.time.Instant;
+import java.util.Iterator;
+
+public class HelloWorldSource implements Source<HelloWorldDocument> {
+
+    private final String greeting;
+
+    public HelloWorldSource(){
+        greeting = "hello, world!";
+    }
+
+    public HelloWorldSource(String greeting){
+        this.greeting = greeting;
+    }
+
+    @Override
+    public Iterator<HelloWorldDocument> getDocuments() {
+
+        return new Yielder<HelloWorldDocument>(){
+            boolean first =true;
+            @Override
+            protected void yieldNextCore() {
+                if (!first){
+                    yieldBreak();
+                    return;
+                }
+                String id = String.valueOf(Instant.now().toEpochMilli());
+                String body = greeting;
+                yieldReturn(new HelloWorldDocument(id, body));
+                first=false;
+            }
+
+        }.iterator();
+    }
+}
